3306ed2e6ee990139b4006963f0573e29c5c4597
Hanaly-dev <hanalyyewhanaly002@gmail.com>
2026-01-15 13:42:20 +0500

    env added

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.filters import Command
 import asyncio
+import os
+from dotenv import load_dotenv
 
-BOT_TOKEN="8279116831:AAFxv-nlkwg5KauuEkbo5IalAuYLlWhbksM"
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot=Bot(token=BOT_TOKEN)
-dp=Dispatcher()
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def handle_start(message: Message):
